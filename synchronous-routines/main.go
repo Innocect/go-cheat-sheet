44566c629f6375a2f98950d933a698d72601faf4
Himanshu Bakshi <55731572+Innocect@users.noreply.github.com>
2023-01-27 14:49:25 +0530

    waitgroup synchronous routine

--- synchronous-routines/main.go
+++ synchronous-routines/main.go
@@ -54,3 +54,59 @@ func myfunc3(signal chan string) {
 		time.Sleep(time.Second)
 	}
 }
+
+
+// using Waitgroups only
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// type myWaitgrp struct {
+// 	counter int
+// 	wg      *sync.WaitGroup
+// }
+
+// // Date should be in the Format MM-DD-YYYY
+// // for single digits add 0 in front of the Month or Date, for example MM -> 02 or DD-> 04 and Year -> 0202
+// // for year `202`.
+
+// func main() {
+// 	var wg1 sync.WaitGroup
+// 	var wg2 sync.WaitGroup
+// 	var wg sync.WaitGroup
+// 	wg.Add(1)
+// 	wg2.Add(1)
+// 	go myfunc1(&wg1, &wg2)
+// 	go myfunc2(&wg1, &wg2, &wg)
+// 	wg.Wait()
+// }
+
+// func myfunc2(waitGroup1 *sync.WaitGroup, waitGroup2, wg *sync.WaitGroup) {
+// 	t := time.Now()
+// 	for {
+// 		waitGroup1.Wait()
+// 		fmt.Print("1st go-rouitne \t")
+// 		waitGroup1.Add(1)
+// 		waitGroup2.Done()
+// 		time.Sleep(time.Second * 2)
+// 		since := time.Since(t).Seconds()
+// 		if time.Duration(since) > 10 {
+// 			wg.Done()
+// 			break
+// 		}
+// 	}
+// }
+
+// func myfunc1(waitGroup1 *sync.WaitGroup, waitGroup2 *sync.WaitGroup) {
+// 	for {
+// 		waitGroup2.Wait()
+// 		fmt.Print("2nd go-rouitne \t")
+// 		waitGroup2.Add(1)
+// 		waitGroup1.Done()
+// 	}
+// }
